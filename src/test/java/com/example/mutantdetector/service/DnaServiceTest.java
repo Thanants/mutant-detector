cd0017a77a3b533d3f598df18f5cd1497281646f
Miguel Oliveras <miguelrauloliveras@gmail.com>
2024-11-02 17:53:17 -0300

    m

--- src/test/java/com/example/mutantdetector/service/DnaServiceTest.java
+++ src/test/java/com/example/mutantdetector/service/DnaServiceTest.java
@@ -1,12 +1,24 @@
 package com.example.mutantdetector.service;
 
 import com.example.mutantdetector.exception.InvalidDnaException;
+import com.example.mutantdetector.repository.DnaRepository;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class DnaServiceTest {
 
-    private final com.example.mutantdetector.service.DnaService dnaService = new com.example.mutantdetector.service.DnaService(null);
+    @Mock
+    private DnaRepository dnaRepository;
+
+    @InjectMocks
+    private com.example.mutantdetector.service.DnaService dnaService;
 
     @Test
     void testIsMutant() {
